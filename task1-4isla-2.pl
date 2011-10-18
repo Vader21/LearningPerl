8540d5998f102d5542ba080b3e18ad9c543f1eea
garm217 <cj.vader21@gmail.com>
2011-10-18 23:29:48 +0300

    изменил названия переменных, убрал ф-цию, упростил запрос по примеру линукс

--- task1-4isla-2.pl
+++ task1-4isla-2.pl
@@ -1,38 +1,37 @@
 #!/usr/bin/perl -w
 use strict;
 my @even_numbers;
-my @not_even_numbers;
+my @odd_numbers;
 my @numbers;
 
-print "Hello, man. This is 4isla 2.0. Do you want to type from you keyboard? y/n \n";
-print "You Answer: \n";
+print "Hello, man. This is 4isla 2.0. Do you want to type from you keyboard? (Y/n): ";
 chomp($_ = <STDIN>);
-if (m/y|yes/i) {
+if (m/y|yes|^$/i) {
     print "Type your numbers with Enter after each number and CNTRL+D in the end \n";
     chomp(@numbers = <STDIN>);
-    eq_or_not();
 } else {
     @numbers = qw/ 20 5 1 351 123 3 5 8 21 24 6 3 34 7 2 6 21 8783 7332 256 /;
-    eq_or_not();}
+}
 
-sub eq_or_not{
 foreach (@numbers) {
     my $new = $_ % 2;
     if ($new) {
-        push (@not_even_numbers, $_)
+	push (@odd_numbers, $_)
     } else {
-         push (@even_numbers, $_)
+	 push (@even_numbers, $_)
     }
 }
+
 print "Massive with Even Numbers. \n";
 print "-" x 29, "\n";
 foreach (@even_numbers) {
     print $_, "\t", "Even Number", "\n";
     print "-" x 29, "\n";
 }
-print "Massive with Not Even Numbers. \n";
+print "Massive with Odd Numbers. \n";
 print "-" x 29, "\n";
-foreach (@not_even_numbers) {
-    print $_, "\t", "Not Even Number", "\n";
+foreach (@odd_numbers) {
+    print $_, "\t", "Odd Number", "\n";
     print "-" x 29, "\n";
-}}
+}
+
