34f5a6f0d96df149f84b5fe7b46754e28139a761
Greya <greya@localhost.localdomain>
2011-10-19 01:52:58 +0300

    New tasks! 0, 2, 3 and 4

---
+++ task0.pl
@@ -0,0 +1,29 @@
+#/usr/bin/perl
+
+=cut
+1. Поставить local::lib
+	http://search.cpan.org/~apeiron/local-lib-1.008004/lib/local/lib.pm#The_bootstrapping_technique
+	Обрати внимание, что там идет по два примера, используй с путем по умолчанию "perl Makefile.PL --bootstrap" и вариант под bash. Не сделай лишнего.
+	Команды, которые будешь исполнять - вставь сюда, чтобы я проверил.
+	Прочитай какую-то статью, чтобы понять зачем оно надо. (статью укажи тут)
+	К следующему пункту переходи, когда пришлешь результат работы и сообщишь, чтобы я проверил.
+
+2. Поставить cpanminus
+	Прочитай, что это такое и зачем надо(поискать статьи), ставить по пункту из ссылки:	
+	http://search.cpan.org/~miyagawa/App-cpanminus-1.5002/lib/App/cpanminus.pm#Installing_to_local_perl_%28perlbrew%29
+
+3. Поставить модуль Task::Dancer
+	Использовать для установки cpanminus (показать КАК) НЕ sudo вариант! наш вариант - perlbrew (local::lib)
+
+	Залить на прочтение в Киндл:
+	Введение: http://search.cpan.org/perldoc?Dancer::Introduction
+	Документация: http://search.cpan.org/perldoc?Dancer
+	Кукбук: http://search.cpan.org/perldoc?Dancer::Cookbook
+	http://perldancer.org/documentation
+
+4. Создать проект на Dancer, назвать его task0-dancer-test запустить и порадоваться
+	http://search.cpan.org/~xsawyerx/Dancer-1.3072/lib/Dancer/Introduction.pod#SETUP
+
+	Прочитать страницу мануала до тех пор, пока станет совсем непонятно.
+	
+=cut
