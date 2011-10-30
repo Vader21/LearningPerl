f3fab514707c86de8fe589c9a4562b77e7126683
garm217 <cj.vader21@gmail.com>
2011-10-30 16:49:20 -0400

    perezalil

--- task2-find-closest-number-1beta.pl
+++ task2-find-closest-number-1beta.pl
@@ -1,30 +1,48 @@
 #!/usr/bin/perl -w
 use strict;
-my @user_numbers;
-my @num_list = qw / 5 10 20 40 80 100 /;
-my @d;
+
+my @numbers;
+my @num_list = qw / 0 5 10 20 30 40 50 60 70 80 90 100 /;
+my @more;
+my @less;
+
 
 #приглашение в программу. выбор печати из масива или с клавиатуры.
-print "Prog 'Find Closest X Coordinate' ver.01 alpha. \n";
-print "X coordinate points at: ".join(",", @num_list);
-print "Do you want to type numbers from keyboard? [y/N]: ";
+print "Prog 'Find Closest Number' ver.01 alpha. \n";
+print "Closest are Numbers 5, 10, 20, 40, 80, 100";
+print "Do you want to type numbers from keyboard? \n";
+print "y/n \n";
+print "You Answer: \n";
 chomp($_ = <STDIN>);
 #печать с клавиатуры
 if (m/y|yes/i) {
     print "Type your numbers with Enter after each number and CNTRL+D in the end \n";
-    chomp(@user_numbers = <STDIN>);
-#печать с массива @user_numbers
+    chomp(@numbers = <STDIN>);
+#печать с массива @numbers
 } else {
-    @user_numbers = qw/ 6 /;
+    @numbers = qw/ 1 15 27 33 /;
 }
-my $d = 0;
-foreach my $x (@user_numbers) {
-    for my $i (0..@num_list) {
-        if ($i == 0) {
-            $d = $x - $num_list[$i];
-        }
-        if (abs($x - $num_list[$i]) <= $d){
-            
-        }
-    } 
+
+foreach my $x (@numbers) {
+    foreach my $y (@num_list) {
+        if ($y > $x) {push (@more, $y)}
+        if ($y < $x) {push (@less, $y)}
+    }
+}
+my $in = 0;
+my $z = -1;
+foreach my $x (@numbers){
+    my $a = abs($x - $more[$in]);
+    my $b = abs($x - $less[$z]);
+   if ($a < $b) {
+    print "$more[$in]" . "\n";
+   }
+   if ($a > $b) {
+    print "$less[$z]" . "\n";
+   }
+   if ($a = $b) {
+    print "$x";
+   }
+   $in ++;
+   $z --; 
 }
