93e15ed765449a137b2b391dfe72866246a1cfd7
Vlad <cj.vader21@gmail.com>
2012-02-13 22:22:08 +0300

    Data. Not Bata

--- task0_PSPerrors/NewPSPErrors.pl
+++ task0_PSPerrors/NewPSPErrors.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl -w
 use strict;
 #PSPerrors Test1.
-print "Hi. Welcome in HQcyber PSP Hack Web Bata Base. You can find all information about Sony errors. They are divided on Sintax, DAX FW ONLY, Networking, Firmware, Memory Stick, UMD, Homebrew, POPS, Other. The categories are in brackets in the end of error description. \n";
+print "Hi. Welcome in HQcyber PSP Hack Web Data Base. You can find all information about Sony errors. They are divided on Sintax, DAX FW ONLY, Networking, Firmware, Memory Stick, UMD, Homebrew, POPS, Other. The categories are in brackets in the end of error description. \n";
 chomp(my $line = <STDIN>);
 open (ERROR, "< errors.txt");
 my %erros_hash = split ('#', <ERROR>);
